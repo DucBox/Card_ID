ad27dfa40424f0a4353949f101066da6e2ef8eed
Ngo Quang Duc <ngoquangduc@192.168.1.8.non-exists.ptr.local>
2025-02-12 00:46:00 +0700

    Change model path in config

--- src/config.py
+++ src/config.py
@@ -6,5 +6,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Định nghĩa đường dẫn model
 MODELS_DIR = os.path.join(BASE_DIR, "models")
 
-CORNER_MODEL_PATH = os.path.join(MODELS_DIR, "best.pt")
+CORNER_MODEL_PATH = os.path.join(MODELS_DIR, "card_detect.pt")
 TEXT_MODEL_PATH = os.path.join(MODELS_DIR, "text_detection.pt")
